9c0805cfc16d1ab7cdd0abed06569562afd71be4
Jeremy Kirkham <jeremy@streame.com.au>
2016-11-23 07:15:14 +1100

    Fix deploy reference to jar

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 lein uberjar
-aws --region us-east-1 lambda update-function-code --function-name deeplakeWebhookEventHandler --zip-file fileb://./target/deeplakeWebhook-standalone.jar
+aws --region us-east-1 lambda update-function-code --function-name deeplakeWebhookEventHandler --zip-file fileb://./target/deeplakeWebhookEventHandler-standalone.jar
