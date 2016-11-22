3a81ce6932a4884c1eac509265a9d1fcb4b6ca71
Jeremy Kirkham <jeremy@streame.com.au>
2016-11-23 08:25:21 +1100

    Fix

--- src/handler.clj
+++ src/handler.clj
@@ -17,7 +17,7 @@
   (let [event (cheshire.core/parse-stream (io/reader in) true)
         res (run event)]
     (with-open [w (io/writer out)]
-      (json/write response w))))
+      (json/write res w))))
 
 ; Test data for running the handler locally.
 (def test-event
